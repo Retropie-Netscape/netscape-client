2919833cfc36eab31c0d610b16a6df5dad703bca
Adam Lynch <alynch14@students.towson.edu>
2019-12-03 18:33:27 -0500

    fix bug

--- ConnectToUser.py
+++ ConnectToUser.py
@@ -13,10 +13,9 @@ def write_connection_details(username: str):
     else:
         with open('/opt/retropie/configs/all/retronetplay.cfg', 'a+') as netplayconfigfile:
             netplayconfigfile.truncate(0)
-            line4 = netplayconfigfile.readline(3)
-            line5 = netplayconfigfile.readline(4)
             lines = ['__netplaymode="C"\n', '__netplayport="' + jsondata['port'] + '"\n',
-            '__neplayhostip="' + jsondata['ipAddress'] + '"\n', line4, line5]
+            '__neplayhostip="' + jsondata['ipAddress'] + '"\n', '__netplayip_cfile=""\n',
+                     '__netplaynickname="' + username + '"\n']
             netplayconfigfile.writelines(lines)
 
     return 0
